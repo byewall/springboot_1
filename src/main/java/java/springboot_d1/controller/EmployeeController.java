aca36f207980bfc8744c9e0ac9698ad8558b2ad2
byewall <bwvhazel@gmail.com>
2023-06-08 15:59:40 +0800

    3rd

---
+++ src/main/java/java/springboot_d1/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package java.springboot_d1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// RequestMapping
+// GetMapping (Retrieve list/single objects)
+// PostMapping (Update an existing object)
+// DeletMapping (Delete an object from a list/db)
+
+@Controller
+@RequestMapping(path="/employees")
+public class EmployeeController {
+    
+}
